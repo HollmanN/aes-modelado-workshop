86d8f1a0cafb91dae2f4c0bc4f5f81bc138f7fa8
Hollman N Castro <hollman_castro@outlook.com>
2018-11-14 09:18:03 -0500

    Added service inventory and Eureka logic

--- soap-consumer-service/src/main/java/edu/javeriana/aes/model/soapconsumerservice/SoapConsumerServiceApplication.java
+++ soap-consumer-service/src/main/java/edu/javeriana/aes/model/soapconsumerservice/SoapConsumerServiceApplication.java
@@ -2,8 +2,10 @@ package edu.javeriana.aes.model.soapconsumerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SoapConsumerServiceApplication {
 
     public static void main(String[] args) {
